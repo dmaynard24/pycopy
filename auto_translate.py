879c5df1f84dc6a6d5a4abe2d2af4f3af32ff4ff
Dave Maynard <dmayna@amazon.com>
2020-11-04 23:00:29 -0500

    added the option for adding categories in the C column of the master workbook

--- auto_translate.py
+++ auto_translate.py
@@ -2,26 +2,25 @@ from openpyxl import load_workbook
 import os, html
 
 # default config, will probably need to tweak a couple of these before running
-default_categories = 'AP'
+default_categories = 'CCFE,APP'
 default_language = 'en_US'
 default_protected = 'false'
-xl_dir = 'C:\\Users\\dmayna\\Desktop\\CareerChoice\\Stories\\US8300\\Final content\\'
-base_out_dir = 'C:\\repos\\ccnextgen-autotranslate\\force-app\\main\\default\\'
+xl_dir = 'C:\\Users\\dmayna\\Desktop\\CareerChoice\\Stories\\US8309\\translations\\'
+base_out_dir = 'C:\\repos\\ccnextgen-sfdx\\force-app\\main\\default\\'
 # always open the "master", this will hold custom label names
 master_wb = load_workbook(
-	filename=f'{xl_dir}Copy of App Flow Content FINAL, ENG - USA.xlsx')
+	filename=f'{xl_dir}App Flow Content FINAL - ENG.xlsx')
 filenames = {
-	'fr_CA': 'Copy of App Flow Content FINAL, FR - CAN',
-	'cs': 'Copy of App Flow Content FINAL, CZ -Czechia',
-	'de': 'Copy of App Flow Content FINAL, DE_Germany',
-	'es': 'Copy of App Flow Content FINAL, ES - ES',
-	'fr': 'Copy of App Flow Content FINAL, FR - FR',
-	'it': 'Copy of App Flow Content FINAL, IT',
-	'pl': 'Copy of App Flow Content FINAL, POL',
-	'sk': 'Copy of App Flow Content FINAL, SK',
-	'es_CR': 'Copy of App Flow Content FINAL, ES - CRI',
+	'fr_CA': 'App Flow Content FINAL, FR - CA',
+	'cs': 'App Flow Content FINAL, CZ',
+	'de': 'App Flow Content FINAL, DE',
+	'es': 'App Flow Content FINAL, ES',
+	'fr': 'App Flow Content FINAL, FR -FR',
+	'it': 'App Flow Content FINAL, IT',
+	'pl': 'App Flow Content FINAL, POL',
+	'sk': 'App Flow Content FINAL, SK',
 }
-sheet_name = '05 Academic Program'
+sheet_name = '06 Payment'
 wb_sheet_master = master_wb[sheet_name]
 row_count = wb_sheet_master.max_row  # last row with content
 
@@ -46,13 +45,14 @@ def write_custom_labels():
 	for row in range(1, row_count + 1):
 		label = wb_sheet_master[f'A{row}'].value
 		name = wb_sheet_master[f'B{row}'].value
+		categories = wb_sheet_master[f'C{row}'].value
 		if label is None or label.strip() == '' or name is None or name.strip(
 		) == '':
 			# empty cell
 			continue
 		label = html.escape(label)
 		xml += f'\t<labels>\n\t\t<fullName>{name}</fullName>'
-		xml += f'\n\t\t<categories>{default_categories}</categories>'
+		xml += f'\n\t\t<categories>{default_categories if categories is None else categories}</categories>'
 		xml += f'\n\t\t<language>{default_language}</language>'
 		xml += f'\n\t\t<protected>{default_protected}</protected>'
 		xml += f'\n\t\t<shortDescription>{name}</shortDescription>'
@@ -72,13 +72,12 @@ def write_translations():
 		wb = load_workbook(filename=f'{xl_dir}{wb_filename}.xlsx')
 		wb_sheet = wb[sheet_name]
 
-		# catch error if this workbook has different number of rows, the data needs to be massaged
+		# catch error if this workbook has different number of rows, the data may need to be massaged
 		wb_sheet_row_count = wb_sheet.max_row
 		if wb_sheet_row_count != row_count:
 			print(
-				f'Error occurred. The {lang} workbook has {wb_sheet_row_count} while the master workbook has {row_count}.'
+				f'Error occurred. The {lang} workbook has {wb_sheet_row_count} while the master workbook has {row_count}. This may not affect the output negatively, sometimes max_row is unpredictable.'
 			)
-			continue
 
 		# go over all rows and create XML nodes for each lang
 		xml = ''
