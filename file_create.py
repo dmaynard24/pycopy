6cf54707edeecf83b86648e36fb29ae26cfb8d5f
Dave Maynard <dmayna@amazon.com>
2021-09-08 13:35:44 -0400

    add util for creating empty files

---
+++ file_create.py
@@ -0,0 +1,17 @@
+import os
+
+
+def file_create(base_dir, filename, dirname_exception=None):
+	# walk directories at the top level of the base directory
+	for _, dirs, _ in os.walk(base_dir):
+		for dir in dirs:
+			# skip directories by name
+			if dirname_exception is None or dir != dirname_exception:
+				# write empty file
+				with open(os.path.join(base_dir, dir, filename), 'w'):
+					pass
+
+
+# change these parameters as needed
+file_create('/Users/dmayna/repos/side/project-euler/python/questions_001_100',
+	'__init__.py', '__pycache__')
--- file_rename.py
+++ file_rename.py
@@ -8,7 +8,7 @@ replace_str = 'Online'
 import os
 
 # walk files and rename them
-for root, dirs, files in os.walk(base_dir):
+for root, _, files in os.walk(base_dir):
 	for file_name in files:
 		new_name = file_name.replace(find_str, replace_str)
 		os.rename(root + '\\' + file_name, root + '\\' + new_name)
